5d2e82c3fa0c987722c79d2dade226b7bac01f8f
Apostolos Bouzalas <apostolos.v.bouzalas@gmail.com>
2023-07-28 13:53:45 +0300

    Added two textures and text.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-// Function to load an image as an SDL_Texture
+// Load an image as an SDL_Texture
 SDL_Texture* loadTexture(SDL_Renderer* renderer, const std::string& path) {
     SDL_Surface* surface = IMG_Load(path.c_str());
     if (!surface) {
@@ -22,7 +22,7 @@ SDL_Texture* loadTexture(SDL_Renderer* renderer, const std::string& path) {
     return texture;
 }
 
-// Function to load a font and render text as an SDL_Texture
+// Load a font and render text as an SDL_Texture
 SDL_Texture* loadFont(SDL_Renderer* renderer, const std::string& fontPath, int fontSize, const std::string& text, SDL_Color textColor) {
     TTF_Font* font = TTF_OpenFont(fontPath.c_str(), fontSize);
     if (!font) {
@@ -39,7 +39,7 @@ SDL_Texture* loadFont(SDL_Renderer* renderer, const std::string& fontPath, int f
 }
 
 int main(int argc, char* args[]) {
-    // Initialize SDL
+    // Initialize SDL or exit early if SDL is not initialized.
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
         return 1;
@@ -48,54 +48,54 @@ int main(int argc, char* args[]) {
     // Initialize SDL_ttf
     if (TTF_Init() != 0) {
         std::cerr << "SDL_ttf initialization failed: " << TTF_GetError() << std::endl;
-        // You can still continue the program if font initialization fails, but font rendering won't work
+        // App will continue if continue the program if font initialization fails, but font rendering won't work
     }
 
     // Initialize SDL_mixer
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
         std::cerr << "SDL_mixer initialization failed: " << Mix_GetError() << std::endl;
-        // You can still continue the program if audio initialization fails, but music won't play
+        // App will continue if audio initialization fails, but music won't play
     }
 
     // Create a window
     SDL_Window* window = SDL_CreateWindow("SDL Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     if (!window) {
         std::cerr << "Window creation failed: " << SDL_GetError() << std::endl;
-        // You can still continue the program if window creation fails, but the window won't show up
+        // App will continue if window creation fails, but the window won't show up
     }
 
     // Create a renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         std::cerr << "Renderer creation failed: " << SDL_GetError() << std::endl;
-        // You can still continue the program if renderer creation fails, but graphics won't be displayed
+        // App will continue if renderer creation fails, but graphics won't be displayed
     }
 
     // Load background textures
     bool texturesLoaded = true;
-    SDL_Texture* upperTexture = loadTexture(renderer, "upper_background.png");
-    SDL_Texture* lowerTexture = loadTexture(renderer, "lower_background.png");
+    SDL_Texture* upperTexture = loadTexture(renderer, "assets/images/upper_background.jpg");
+    SDL_Texture* lowerTexture = loadTexture(renderer, "assets/images/lower_background.jpg");
     if (!upperTexture || !lowerTexture) {
         std::cerr << "Failed to load background textures." << std::endl;
         texturesLoaded = false;
-        // You can still continue the program if background texture loading fails, but the textures won't be displayed
+        // App will continue if background texture loading fails, but the textures won't be displayed
     }
 
     // Load font and render "SDL Template" text
     bool fontLoaded = true;
-    TTF_Font* font = TTF_OpenFont("Roboto-Regular.ttf", 36);
+    TTF_Font* font = TTF_OpenFont("assets/fonts/RobotoRegular.ttf", 36);
     if (!font) {
         std::cerr << "Error loading font: " << TTF_GetError() << std::endl;
         fontLoaded = false;
-        // You can still continue the program if font loading fails, but the text won't be displayed
+        // App will continue if the program if font loading fails, but the text won't be displayed
     }
 
     SDL_Color textColor = { 255, 255, 255, 255 }; // White color for text
-    SDL_Texture* textTexture = loadFont(renderer, "Roboto-Regular.ttf", 36, "SDL Template", textColor);
+    SDL_Texture* textTexture = loadFont(renderer, "assets/fonts/RobotoRegular.ttf", 36, "SDL Template", textColor);
     if (!textTexture) {
         std::cerr << "Failed to render text." << std::endl;
         fontLoaded = false;
-        // You can still continue the program if text rendering fails, but the text won't be displayed
+        // App will continue if the program if text rendering fails, but the text won't be displayed
     }
 
     // Set the blend mode to enable transparency
@@ -114,23 +114,15 @@ int main(int argc, char* args[]) {
     int textX = (SCREEN_WIDTH - textWidth) / 2;
     int textY = (SCREEN_HEIGHT - textHeight) / 2;
 
-    // Load music
-    Mix_Music* music = Mix_LoadMUS("music.mp3");
-    if (!music) {
-        std::cerr << "Failed to load music: " << Mix_GetError() << std::endl;
-        // You can still continue the program if music loading fails, but music won't play
-    } else {
-        // Play music if loaded successfully
-        Mix_PlayMusic(music, -1); // The second parameter (-1) means loop indefinitely
-    }
-
-    // Fade out effect (reduce alpha from 255 to 0)
+    // Fade in effect (increase alpha from 0 to 255)
     if (texturesLoaded && fontLoaded) {
-        for (int alpha = 255; alpha >= 0; alpha -= 5) {
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, alpha);
+        for (int alpha = 0; alpha <= 255; alpha += 5) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderClear(renderer);
 
             // Render upper and lower background textures
+            SDL_SetTextureAlphaMod(upperTexture, alpha);
+            SDL_SetTextureAlphaMod(lowerTexture, alpha);
             SDL_RenderCopy(renderer, upperTexture, nullptr, nullptr);
             SDL_RenderCopy(renderer, lowerTexture, nullptr, nullptr);
 
@@ -139,8 +131,29 @@ int main(int argc, char* args[]) {
             SDL_Rect textRect = { textX, textY, textWidth, textHeight };
             SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
 
-            SDL_RenderPresent(renderer);
+             SDL_RenderPresent(renderer);
             SDL_Delay(50); // Delay to control fade speed (adjust as needed)
+        }
+    }
+
+    // Load music
+    Mix_Music* music = Mix_LoadMUS("music.mp3");
+    if (!music) {
+        std::cerr << "Failed to load music: " << Mix_GetError() << std::endl;
+        // App will continue if the program if music loading fails, but music won't play
+    } else {
+        // Play music if loaded successfully
+        Mix_PlayMusic(music, -1); // The second parameter (-1) means loop indefinitely
+    }
+
+    // Event loop to keep the window open until the user clicks the top-right X button
+    SDL_Event event;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
         }
     }
 
@@ -163,6 +176,6 @@ int main(int argc, char* args[]) {
 
     // Quit SDL
     SDL_Quit();
-	
-	return 0;
+
+    return 0;
 }
